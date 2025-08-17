850e5d3e1eb13ee3302d5aa1b20d04456b6db36e
anne-bode <113382612+anne-bode@users.noreply.github.com>
2025-08-17 12:21:18 -0400

    Ab/pulling inflation data (#6)
    * Update X share button to match brand color (#5245)
    
    * Update CHANGELOG and history
    
    * Additional HTML escaping for titles and metadata (#5276)
    
    * Hide page__footer-follow if there are no links nor atom feed. (#5279)
    
    Co-authored-by: iBug <git@ibugone.com>
    
    * Optional `footer.since` for copyright time range (#5275)
    
    Co-authored-by: iBug <git@ibugone.com>
    
    * Update history from CHANGELOG
    
    * Add `escape_once` to gallery title and alt text
    
    * Release 4.27.2 💎
    
    * Add CSS for .sr-only
    
    So we're not relying on Font Awesome 6-
    
    * Release 4.27.3 💎
    
    * Updated Traditional Chinese translation (#5250)
    
    * Update CHANGELOG and history
    
    * Delete Travis CI config
    
    * missed a merge conflict somehow
    
    * pulling inflation data
    
    * remove pycache generation
    
    * remove accidental updates
    
    * ignore secret file
    
    * remove secret file
    
    * add title to inflation analysis
    
    * delete ds store which accidentally got through
    
    ---------
    
    Co-authored-by: Yosuke Kawaguchi <44657956+y-kawa@users.noreply.github.com>
    Co-authored-by: iBug <git@ibugone.com>
    Co-authored-by: Pander <198964+PanderMusubi@users.noreply.github.com>
    Co-authored-by: Keyang556 <65295310+keyang556@users.noreply.github.com>

---
+++ data/bureau_of_labor_statistics/api_bls.py
@@ -0,0 +1,43 @@
+import sys
+import requests
+import json
+import pandas as pd
+import time
+from data.bureau_of_labor_statistics.secret_keys import bls_api_key
+
+sys.dont_write_bytecode = True # prevents the creation of a pycache folder for secrets
+
+headers = {'Content-type': 'application/json'}
+
+def get_bls_data(series_ids: list[str], years: list[int]):
+    df = pd.DataFrame(columns=["series id", "year", "period", "value"])
+    
+    for y in range(0, len(years), 20):
+        for s in range(0, len(series_ids), 50):
+            data = json.dumps(
+                {
+                    "seriesid": series_ids[s:min(s+49, len(series_ids))],
+                    "startyear": years[y],
+                    "endyear": years[min(y+19, len(years)-1)],
+                    "registrationkey": bls_api_key,
+                }
+            )
+            p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+            json_data = json.loads(p.text)
+            for series in json_data['Results']['series']:
+                seriesId = series['seriesID']
+                for item in series['data']:
+                    year = item['year']
+                    period = item['period']
+                    value = item['value']
+                    if 'M01' <= period <= 'M12':
+                        df.loc[len(df)] = [seriesId, year, period, value]
+            
+            time.sleep(1)
+
+    return df
+
+# Test the function
+if __name__ == "__main__":
+    print("Script loaded successfully!")
+    print("Function 'get_bls_data' is ready to use.")
